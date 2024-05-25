5e00a3572ec8905e69891d040cb21b2b280cce65
Amir Adib <amir29adib1234@gmail.com>
2024-05-25 11:19:34 +0330

    define base model and create booking model with relation with user

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from core.models import BaseModel
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(BaseModel):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField()
